c0dcc2ee4150c69f42cd90d0726a0b654aa27971
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-07 18:46:07 +0200

    Finished PolandMap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from "./components/Header/Header";
 import {PolandMap,  } from './components/PolandMap/PolandMap';
 import {Voivodeship, } from "./components/Voivodeship/Voivodeship";
 
 export const App = () => {
-  return (
-    <div className="App">
-        <Header title={"Aplikacja pogodowa"}/>
-        <main>
-            <PolandMap>
-                <Voivodeship voivodeship={"dolnoslaskie"}/>
-                <Voivodeship voivodeship={"kujawsko-pomorskie"}/>
-                <Voivodeship voivodeship={"lubelskie"}/>
-                <Voivodeship voivodeship={"lubuskie"}/>
-                <Voivodeship voivodeship={"lodzkie"}/>
-                <Voivodeship voivodeship={"malopolskie"}/>
-                <Voivodeship voivodeship={"mazowieckie"}/>
-                <Voivodeship voivodeship={"opolskie"}/>
-                <Voivodeship voivodeship={"podkarpackie"}/>
-                <Voivodeship voivodeship={"podlaskie"}/>
-                <Voivodeship voivodeship={"pomorskie"}/>
-                <Voivodeship voivodeship={"slaskie"}/>
-                <Voivodeship voivodeship={"swietokrzyskie"}/>
-                <Voivodeship voivodeship={"warminsko-mazurskie"}/>
-                <Voivodeship voivodeship={"wielkopolskie"}/>
-                <Voivodeship voivodeship={"zachodniopomorskie"}/>
-            </PolandMap>
-        </main>
+    const [voivodeship, setVoivodeship] = useState("");
+    return (
+        <div className="App">
+            <Header title={"Aplikacja pogodowa"}/>
+            <main>
+                <PolandMap>
+                    <Voivodeship voivodeship={"dolnoslaskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"kujawsko-pomorskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"lubelskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"lubuskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"lodzkie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"malopolskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"mazowieckie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"opolskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"podkarpackie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"podlaskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"pomorskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"slaskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"swietokrzyskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"warminsko-mazurskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"wielkopolskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                    <Voivodeship voivodeship={"zachodniopomorskie"} isSelect={voivodeship} setSelect={setVoivodeship}/>
+                </PolandMap>
+            </main>
+        <p>{voivodeship}</p>
     </div>
   );
 }
