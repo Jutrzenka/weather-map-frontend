11cb9dddefac1381bbbbb18d172668b03392f000
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-07 16:28:01 +0200

    Fixed map to responsive version, created all voivodeships, added :hover to voivodeships and added onClick function to Voivodeship.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import {Header} from "./components/Header/Header";
-import { PolandMap } from './components/PolandMap/PolandMap';
-import {Voivodeship} from "./components/Voivodeship/Voivodeship";
+import {PolandMap,  } from './components/PolandMap/PolandMap';
+import {Voivodeship, } from "./components/Voivodeship/Voivodeship";
 
 export const App = () => {
   return (
     <div className="App">
         <Header title={"Aplikacja pogodowa"}/>
-        <PolandMap>
-            <Voivodeship voivodeship={"mazowieckie"}/>
-            <Voivodeship voivodeship={"podlaskie"}/>
-        </PolandMap>
+        <main>
+            <PolandMap>
+                <Voivodeship voivodeship={"dolnoslaskie"}/>
+                <Voivodeship voivodeship={"kujawsko-pomorskie"}/>
+                <Voivodeship voivodeship={"lubelskie"}/>
+                <Voivodeship voivodeship={"lubuskie"}/>
+                <Voivodeship voivodeship={"lodzkie"}/>
+                <Voivodeship voivodeship={"malopolskie"}/>
+                <Voivodeship voivodeship={"mazowieckie"}/>
+                <Voivodeship voivodeship={"opolskie"}/>
+                <Voivodeship voivodeship={"podkarpackie"}/>
+                <Voivodeship voivodeship={"podlaskie"}/>
+                <Voivodeship voivodeship={"pomorskie"}/>
+                <Voivodeship voivodeship={"slaskie"}/>
+                <Voivodeship voivodeship={"swietokrzyskie"}/>
+                <Voivodeship voivodeship={"warminsko-mazurskie"}/>
+                <Voivodeship voivodeship={"wielkopolskie"}/>
+                <Voivodeship voivodeship={"zachodniopomorskie"}/>
+            </PolandMap>
+        </main>
     </div>
   );
 }
