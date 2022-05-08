ec823ae75ece4f93dfd6a346a2b74a0783095373
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-09 01:39:46 +0200

    The main element of the website has been completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,26 @@ import React, {useState} from 'react';
 import {Header} from "./components/Header/Header";
 import {useFetch, } from './utils/useFetch';
 import {Map} from "./components/Map/Map";
+import { Separator } from './components/Separator/Separator';
+import { CityMain } from './components/CityMain/CityMain';
+import {ButtonLink} from "./components/ButtonLink/ButtonLink";
 
 export const App = () => {
     const [voivodeship, setVoivodeship] = useState<string | null>(null);
     const [isLoaded, fetchData] = useFetch(voivodeship);
 
-    let synopticData;
-    let info = null;
-    let hydroData;
-    if (fetchData !== null && isLoaded) {
-        info = <h3>Poziom wody:</h3>;
-        synopticData = [...fetchData.synopticData].map((value) =>
-            <li>
-                <h2>{value.city}</h2>
-                <p>{value.temp}</p>
-                <p>{value.atmosphericPressure}</p>
-                <p>{value.relativeHumidity}</p>
-                <p>{value.totalPrecipitation}</p>
-            </li>);
-        hydroData = [...fetchData.hydroData].map((value) =>
-            <li>
-                <p>{value.city} - {value.river} - {value.waterHeight}</p>
-            </li>
-        )
-    } else if (voivodeship !== null && !isLoaded) {
-        synopticData = "Ładowanie...";
-    } else {
-        synopticData = "Wybierz województwo";
-    }
-
     return (
         <div className="App">
-            <Header title={"Aplikacja pogodowa"}/>
-            <h1>{fetchData !== null ? fetchData.name : "Wybierz województwo"}</h1>
-            <Map voivodeship={voivodeship} setVoivodeship={setVoivodeship}/>
-            <ul>{synopticData}</ul>
-            {info}
-            <ul>{hydroData}</ul>
-    </div>
-  );
-}
+            <main className={"App_main"}>
+                <Header title={"Aplikacja pogodowa"}/>
+                <Separator/>
+                <div className={"App__info"}>
+                    <Map voivodeship={voivodeship} setVoivodeship={setVoivodeship}/>
+                    <CityMain isLoaded={isLoaded} cityData={fetchData}/>
+                </div>
+                <Separator/>
+                <ButtonLink href={"https://danepubliczne.imgw.pl"} text={"link do źródła danych"}/>
+            </main>
+        </div>
+    );
+};
